a0b3faea2ceeb1d316a9e5f362bf9b9ab3f915b2
Vendula Masarova <vendula.masarova@wdf.cz>
2023-04-03 22:27:04 +0200

    feat: delete feature and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React, { useState } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
-import { Popover } from "bootstrap";
 import { DateSelectArg, EventClickArg, EventContentArg } from '@fullcalendar/core';
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 
 const App = () => {
     let id = 0;
+
+    const createEventId = () => {
+        return String(id++)
+    }
     const [initialEvents, setInitialEvents] = useState([
         {
-            id: String(1),
+            id: createEventId(),
 			title: 'Trip to Dolomites',
 			start: '2023-04-06',
 			end: '2023-04-10',
 			color: "#eff5f9",
+            backgroundColor: '#ffe1b9',
+            textColor: '#77340c',
+            expandRows: '3',
+            eventMinHeight: '20px',
+            displayEventEnd: true,
+        },
+        {
+            id: createEventId(),
+			title: 'Submit an interview assignment',
+			start: '2023-04-10',
+			color: "#eff5f9",
+            backgroundColor: '#cfefa9',
+            textColor: '#5f8fa1',
+            allDay: false
         }
     ])
+    console.log(initialEvents)
     const renderEventContent=(eventContent:EventContentArg) => {
         return(
             <>
@@ -28,13 +47,13 @@ const App = () => {
             </>
         )
     }
-    const handleEventClick = (clickInfo:EventClickArg) => {
-        alert(`you clicked ${clickInfo.event.title}`)
-        console.log(...initialEvents)
-    }
+    // const handleEventClick = (clickInfo:EventClickArg) => {
+    //     alert(`you clicked ${clickInfo.event.title}`)
+    //     console.log(...initialEvents)
+    // }
 
     const handleDataSelect = (selectInfo:DateSelectArg) => {
-        let title = prompt('Event sdfs');
+        let title = prompt('Add new event');
         let calendarApi = selectInfo.view.calendar
         calendarApi.unselect();
         if(title) {
@@ -47,20 +66,49 @@ const App = () => {
             })
         }
     }
+
+    const handleEventClick = (clickInfo) => {
+        let text = `Are you sure you want to delete the event '${clickInfo.event.title}'`
+    if (window.confirm(text)){
+      clickInfo.event.remove()
+    }
+
+    const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: id++,
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+  }
   return (
     <>
       <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrap5Plugin,]}
+      themeSystem='bootstrap5'
+      timeZone='UTC'
       headerToolbar={{
         left:"prev, next today",
         center: "title",
         right:"dayGridMonth, timeGridWeek, timeGridDay"
       }}
-      dayMaxEvents={true}
       initialView="dayGridMonth"
-      eventContent={renderEventContent}
+      selectMirror={true}
+      dayMaxEvents={true}
       initialEvents={initialEvents}
-      events={initialEvents}
+      eventBackgroundColor='#d5d5fd'
+      eventTextColor='#493696'
+      eventBorderColor='#d5d5fd'
+      eventDisplay='block'
       editable={true}
       selectable={true}
       locale={'cs'}
@@ -68,18 +116,9 @@ const App = () => {
       dateClick={(e: DateClickArg) => {
         console.log('dateclick', e)
       }}
+      eventContent={renderEventContent}
       select={handleDataSelect}
       eventClick={handleEventClick}
-      eventDidMount={(info) => {
-        return new Popover(info.el, {
-            placement: "auto",
-            trigger: "hover",
-            customClass: "popoverStyle",
-            html: true,
-            content: "<p>Hello</p>"
-            }      
-        )
-      }}
       />
     </>
   );
